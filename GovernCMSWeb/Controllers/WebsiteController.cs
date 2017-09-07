36b17a7a0e074f67fd05dd851a1a268ef0ee65da
Philip Tenn <ptenn@users.noreply.github.com>
2017-09-07 08:39:42 -0400

    Save of BreadCrumb Categories working.

--- GovernCMSWeb/Controllers/WebsiteController.cs
+++ GovernCMSWeb/Controllers/WebsiteController.cs
@@ -152,10 +152,10 @@ namespace GovernCMS.Controllers
         }
 
         [HttpPost]
-        public ActionResult SaveCategories(int websiteId, IList<Category> categories)
+        public ActionResult SaveCategories(int websiteId, string categoriesJson)
         {
             // Now, create all new Sections and Items, providing the Agenda Id for Referential Integrity
-//            IList<Category> categories = JsonConvert.DeserializeObject<IList<Category>>(categoriesAsJson);
+            IList<Category> categories = JsonConvert.DeserializeObject<IList<Category>>(categoriesJson);
 
             categories = ProcessCategories(websiteId, categories);
 
